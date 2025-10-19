5c5c91af8e78e102163681ddcb2f3a0fee1ee9e3
BlexArown <pastukhovaleksandr98@gmail.com>
2025-10-20 06:13:31 +0700

    Сделал рефакторинг приложения, добавил файлы из прошлых практических

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.calculator
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_calculator).setOnClickListener {
+            val intent = Intent(this, CalculatorActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.btn_player).setOnClickListener {
+            // позже
+        }
+
+        findViewById<Button>(R.id.btn_location).setOnClickListener {
+            // позже
+        }
+
+        findViewById<Button>(R.id.btn_telephony).setOnClickListener {
+            // позже
+        }
+
+        findViewById<Button>(R.id.btn_sockets).setOnClickListener {
+            // позже
+        }
+
+        findViewById<Button>(R.id.btn_views).setOnClickListener {
+            // позже
+        }
+    }
+}
